d2d82d7ff8a20bc887196b3f9c32985ff611ccaa
Areyes116 <adrian.reyes2006@icloud.com>
2024-09-03 10:56:29 -0700

    Done

--- problem2.java
+++ problem2.java
@@ -2,7 +2,7 @@ public class Problem2
 {
   public static void main(String[] args)
   {
-    System.out.print("Coding is fun! ");
+    System.out.println("Coding is fun! ");
     System.out.println("Coding is fun! ");
     System.out.print("Coding is fun!");
   }
--- problem3.java
+++ problem3.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.print("=========");
+    System.out.println("=========");
     System.out.println("= 0   0 =");
     System.out.print("=========");
   }

70fbba9fbf4cc533262b9d59a56328b6ca34999a
Areyes116 <adrian.reyes2006@icloud.com>
2024-09-03 11:10:57 -0700

    Blank

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,13 @@ public class HelloWorld
   public static void main(String[] args)
   {
     System.out.print("Hello world!");
+    System.out.print("Hello world!");
+
+    int myAge;
+    myAge = 17
+
+    double price = 4.99;
+    String myName = "Adrian Reyes";
   }
 }
  
